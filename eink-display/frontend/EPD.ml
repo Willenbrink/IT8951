410082c1b7473af088698c036bfcf0e35d37f4b6
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2019-04-11 12:59:31 +0200

    Added out-of-bounds checks. Without these the system can freeze unrecoverably. Perhaps overwriting the wrong memory overwrites code in the IT8951 or corrupts its state in another way resulting in it freezing. The Raspberry Pi then waits for an infinite amount of time for the response. (Suspected cause, not tested!) Using the reset button on the IT8951 might resolve the problem.

--- eink-display/frontend/EPD.ml
+++ eink-display/frontend/EPD.ml
@@ -18,7 +18,9 @@ let bg = 0xFF
 let width () = funer "width" (void @-> returning int) ()
 let height () = funer "height" (void @-> returning int) ()
 
-let check (x,y) = x >= 0 && x < width () && y >= 0 && y < height ()
+let check (x,y) = if x < 0 || x >= width () || y < 0 || y >= height () then raise (Invalid_argument "Point outside of screen")
+
+let check_area (p1,p2) = check p1; check p2
 
 (* Returns true on failure *)
 let init () =
@@ -29,9 +31,13 @@ let free () = funer "freeGraphics" vv ()
 
 let load_image t = funer "loadImage" (ptr void @-> returning void) t
 
-let plot (x,y) = if check (x,y) then funer "plot" (int @-> int @-> returning void) x y
+let plot (x,y) =
+  check (x,y);
+  funer "plot" (int @-> int @-> returning void) x y
 
-let point_color (x,y) = funer "pointColor" (int @-> int @-> returning int) x y
+let point_color (x,y) =
+  check (x,y);
+  funer "pointColor" (int @-> int @-> returning int) x y
 
 let set_color c = funer "setColor" (int @-> returning void) c
 
@@ -39,43 +45,59 @@ let clear c = funer "clearColor" (int @-> returning void) c
 
 let get_screen () = (0,0),(width () - 1, height () - 1)
 
-let load_image img ((x1,y1),(x2,y2)) = funer "loadImage" (ptr void @-> int @-> int @-> int @-> int @-> returning void) img x1 y1 x2 y2
+let load_image img ((x1,y1),(x2,y2) as a) =
+  check_area a;
+  funer "loadImage" (ptr void @-> int @-> int @-> int @-> int @-> returning void) img x1 y1 x2 y2
 
-let display ((x1,y1),(x2,y2)) mode = funer "display" (int @-> int @-> int @-> int @-> int @-> returning void) x1 y1 x2 y2 (mode_to_int mode)
+let display ((x1,y1),(x2,y2) as a) mode =
+  check_area a;
+  funer "display" (int @-> int @-> int @-> int @-> int @-> returning void) x1 y1 x2 y2 (mode_to_int mode)
 
-let display_buffer ((x1,y1),(x2,y2)) mode = funer "displayBuffer" (int @-> int @-> int @-> int @-> returning void) x1 y1 x2 y2
+let display_buffer ((x1,y1),(x2,y2) as a) mode =
+  check_area a;
+  funer "displayBuffer" (int @-> int @-> int @-> int @-> returning void) x1 y1 x2 y2
 
 let display_all mode = display (get_screen ()) mode
 
 let display_buffer_all mode = display_buffer (get_screen ()) mode
 
+let draw_line ((x1,y1),(x2,y2) as a) =
+  check_area a;
+  funer "drawLine" (int @-> int @-> int @-> int @-> returning void) x1 y1 x2 y2
 
-let draw_line (x1,y1) (x2,y2) = if check (x1,y1) && check (x2,y2) then funer "drawLine" (int @-> int @-> int @-> int @-> returning void) x1 y1 x2 y2
+let draw_text ?(fg=fg) ?(bg=bg) (x,y as p) str =
+  (*TODO check area instead of point <- calculate area of char beforehand*)
+  check p;
+  funer "putText" (int @-> int @-> string @-> int @-> int @-> returning void) x y str fg bg
 
-let draw_text ?(fg=fg) ?(bg=bg) (x,y) str = funer "putText" (int @-> int @-> string @-> int @-> int @-> returning void) x y str fg bg
+let draw_circle (x,y) r =
+  check (x-r,y-r);
+  check (x+r,y+r);
+  funer "drawCircle" (int @-> int @-> int @-> returning void) x y r
 
-let draw_circle (x,y) r = funer "drawCircle" (int @-> int @-> int @-> returning void) x y r
+let fill_circle (x,y) r =
+  check (x-r,y-r);
+  check (x+r,y+r);
+  funer "fillCircle" (int @-> int @-> int @-> returning void) x y r
 
-let fill_circle (x,y) r = funer "fillCircle" (int @-> int @-> int @-> returning void) x y r
+let draw_rect ((x1,y1),(x2,y2) as a) =
+  check_area a;
+  draw_line ((x1,y1),(x2,y1));
+  draw_line ((x2,y1),(x2,y2));
+  draw_line ((x2,y2),(x1,y2));
+  draw_line ((x1,y2),(x1,y1))
 
-let draw_rect (x1,y1) (x2,y2) =
-  draw_line (x1,y1) (x2,y1);
-  draw_line (x2,y1) (x2,y2);
-  draw_line (x2,y2) (x1,y2);
-  draw_line (x1,y2) (x1,y1)
-
-let rec fill_rect (x1,y1) (x2,y2) =
-  draw_line (x1,y1) (x2,y1);
+let rec fill_rect ((x1,y1),(x2,y2) as a) =
+  check_area a;
+  draw_line ((x1,y1),(x2,y1));
   if y1 < y2 then
-    fill_rect (x1,y1+1) (x2,y2)
+    fill_rect ((x1,y1+1),(x2,y2))
 
 let rgb r g b = funer "rgb" (int @-> int @-> int @-> returning int) r g b
 
-
-
 let random int = Random.int int
 
-let point () = random 799, random 599
+let point () = random (width () -1), random (height () -1)
 
 let char () = random 255 |> Char.chr
 
